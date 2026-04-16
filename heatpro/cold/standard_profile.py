4f227c529548827df7495a1f44bf6b402f8fd0fd
RobinsonBeaucour <robinson.beaucour@cea.fr>
2026-04-16 14:23:29 +0200

    Cold Profile Librairy

---
+++ heatpro/cold/standard_profile.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from enum import Enum
+from typing import Callable
+
+import pandas as pd
+
+from ..week_profile import smooth_step_night_reduction_week_profile
+
+
+@dataclass
+class WeekProfile:
+    description: str
+    function: Callable[[int, int], int]
+
+    @property
+    def week_series_example(self):
+        return pd.Series(
+            {
+                date: self.function(date.day_of_week, date.hour)
+                for date in pd.date_range("2025-01-06", freq="1h", periods=7 * 24)
+            },
+            name="week_profile_weigth",
+        )
+
+
+WORKING_DAY_PROFILE = WeekProfile(
+    """Weekly consumption profile with consumption during working hours
+only on working days.
+
+This profile is well adapted for tertiary buildings without weekend activities
+such as schools, offices, and administrations...""",
+    smooth_step_night_reduction_week_profile(0.2, 0.25, 1),
+)
+
+FULL_WEEK_PROFILE = WeekProfile(
+    """Weekly consumption profile with consumption during working hours
+all week long.
+
+This profile is well adapted for buildings in operation all week long
+such as EPHAD, hospitals, shops, and hotels...""",
+    smooth_step_night_reduction_week_profile(0.2, 0.9, 1),
+)
+
+
+class StandardProfile(Enum):
+    WORKING_DAY = WORKING_DAY_PROFILE
+    FULL_WEEK = FULL_WEEK_PROFILE
--- heatpro/week_profile.py
+++ heatpro/week_profile.py
@@ -17,6 +17,28 @@ def step_night_reduction_week_profile(
     return week_profile
 
 
+def smooth_step_night_reduction_week_profile(
+    low: float, high_weekend: float, high_weekstart: float = 1.0
+) -> Callable[[int, int], int]:
+    def week_profile(day: int, hour: int) -> float:
+        if day in [5, 6]:  # On Weekend
+            if 10 <= hour <= 17:
+                return high_weekend
+            if hour == 9 or hour == 18:
+                return 1 / 4 * low + 3 / 4 * high_weekend
+            if hour == 8 or hour == 19:
+                return 3 / 4 * low + 1 / 4 * high_weekend
+        if 10 <= hour <= 17:  # On Weekstart
+            return high_weekstart
+        if hour == 9 or hour == 18:
+            return 1 / 4 * low + 3 / 4 * high_weekstart
+        if hour == 8 or hour == 19:
+            return 3 / 4 * low + 1 / 4 * high_weekstart
+        return low
+
+    return week_profile
+
+
 def apply_week_profile(
     datetime_index: pd.DatetimeIndex, week_profile: Callable[[int, int], int]
 ) -> pd.Series:
