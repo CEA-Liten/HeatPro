af063bf2122337f9ad1de430fa6eadc78f0d2a05
RobinsonBeaucour <robinson.beaucour@cea.fr>
2026-04-16 15:56:35 +0200

    ajout Enum class pour batiment et zone climatique
    Servira plus tard pour calculer une demande annuelle de froid

---
+++ heatpro/building_type.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+
+class BuildingType(Enum):
+    OFFICE = "Bureaux"
+    CAFE = "Café"
+    RETAIL = "Commerces"
+    EHPAD = "EHPAD"
+    SCHOOL = "Écoles"
+    HOTEL = "Hôtel"
+    RESIDENTIAL = "Résidentiel"
+    HEALTHCARE = "Santé"
+    SPORTS = "Sport"
---
+++ heatpro/climatic_zone.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class ClimaticZone(Enum):
+    H1A = "H1a"
+    H1B = "H1b"
+    H1C = "H1c"
+    H2A = "H2a"
+    H2B = "H2b"
+    H2C = "H2c"
+    H2D = "H2d"
+    H3 = "H3"
