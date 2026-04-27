7df9e5b095fe44c6f24d72586f603241f17d77a6
RobinsonBeaucour <robinson.beaucour@cea.fr>
2026-04-27 17:06:35 +0200

    New cold profile from nPro

--- heatpro/cold/standard_profile.py
+++ heatpro/cold/standard_profile.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from enum import Enum
+from pathlib import Path
 from typing import Callable
 
 import pandas as pd
@@ -39,13 +40,40 @@ class WeekProfile:
         )
 
 
+STEP_WORKING_DAY_PROFILE = WeekProfile(
+    """Weekly consumption profile with consumption during working hours
+only on working days.
+
+    This profile is adapted for tertiary buildings without weekend activities
+    such as schools, offices, and administrations.""",
+    smooth_step_night_reduction_week_profile(0.2, 0.25, 1),
+)
+
+STEP_FULL_WEEK_PROFILE = WeekProfile(
+    """Weekly consumption profile with step consumption during working hours
+all week long.
+
+    This profile is adapted for buildings in operation all week long
+    such as EPHAD, hospitals, shops, and hotels.""",
+    smooth_step_night_reduction_week_profile(0.2, 0.9, 1),
+)
+
+DATA_WORKING_DAY_PROFIL = pd.read_csv(
+    Path(__file__).parent / "profile_working_day.csv", sep=";", index_col=[0, 1]
+)
+
 WORKING_DAY_PROFILE = WeekProfile(
     """Weekly consumption profile with consumption during working hours
 only on working days.
 
     This profile is well adapted for tertiary buildings without weekend activities
-    such as schools, offices, and administrations.""",
-    smooth_step_night_reduction_week_profile(0.2, 0.25, 1),
+    such as schools, offices, and administrations.
+    Source: https://www.npro.energy/main/en/load-profiles/office""",
+    lambda day, hour: DATA_WORKING_DAY_PROFIL.loc[(day, hour), "value"],
+)
+
+DATA_FULL_WEEK_PROFILE = pd.read_csv(
+    Path(__file__).parent / "profile_full_week.csv", sep=";", index_col=[0, 1]
 )
 
 FULL_WEEK_PROFILE = WeekProfile(
@@ -53,8 +81,9 @@ FULL_WEEK_PROFILE = WeekProfile(
 all week long.
 
     This profile is well adapted for buildings in operation all week long
-    such as EPHAD, hospitals, shops, and hotels.""",
-    smooth_step_night_reduction_week_profile(0.2, 0.9, 1),
+    such as EPHAD, hospitals, shops, and hotels.
+    Source: https://www.npro.energy/main/en/load-profiles/shopping-center""",
+    lambda day, hour: DATA_FULL_WEEK_PROFILE.loc[(day, hour), "value"],
 )
 
 
@@ -62,9 +91,15 @@ class StandardProfile(Enum):
     """An enumeration of standard weekly consumption profiles.
 
     Attributes:
-        WORKING_DAY (WeekProfile): Profile with consumption only on working days.
-        FULL_WEEK (WeekProfile): Profile with consumption all week long.
+        STEP_WORKING_DAY (WeekProfile): Profile with step consumption only on working days.
+        STEP_FULL_WEEK (WeekProfile): Profile with step consumption all week long.
     """
 
+    STEP_WORKING_DAY = STEP_WORKING_DAY_PROFILE
+    STEP_FULL_WEEK = STEP_FULL_WEEK_PROFILE
     WORKING_DAY = WORKING_DAY_PROFILE
     FULL_WEEK = FULL_WEEK_PROFILE
+
+
+if __name__ == "__main__":
+    print(DATA_WORKING_DAY_PROFIL)
